c7635c3b5f955424d3537014f0209501b122107b
Josh Bowling <joshuaebowling@gmail.com>
2020-12-25 18:13:44 -0400

    add lodash for assign, can i export a function to update a property's value?

--- src/useValidate.tsx
+++ src/useValidate.tsx
@@ -1,9 +1,13 @@
 /// <reference path="index.d.ts" />
 import { useState, useEffect } from "react";
-
+import { assign } from "lodash";
 function useValidate(validate, model) {
   const [currentModel, setCurrentModel] = useState(model);
   const [isValid, setIsValid] = useState(validate());
+  const update = (prop, value) => {
+    const newCurrentModel = assign({}, currentModel, { prop: value });
+    setCurrentModel(newCurrentModel);
+  };
   useEffect(() => {
     setIsValid(validate());
   }, [currentModel]);
