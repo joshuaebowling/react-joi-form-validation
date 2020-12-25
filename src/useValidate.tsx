eaeb954cc1908f887d74211ac0ff289471f34834
Josh Bowling <joshuaebowling@gmail.com>
2020-12-25 08:34:46 -0400

    implement hook [WIP]

--- src/useValidate.tsx
+++ src/useValidate.tsx
@@ -1,7 +1,12 @@
-import { useState } from "react";
+/// <reference path="index.d.ts" />
+import { useState, useEffect } from "react";
 
 function useValidate(validate, model) {
-  const [currentModel, setCurrentModel] = useState();
+  const [currentModel, setCurrentModel] = useState(model);
+  const [isValid, setIsValid] = useState(validate());
+  useEffect(() => {
+    setIsValid(validate());
+  }, [currentModel]);
 }
 
 export default useValidate;
